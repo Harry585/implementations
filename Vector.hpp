3af0130c4b34fe90220c42a35f93b1df3fe9916a
Harry Wu <u7567973@anu.edu.au>
2025-01-28 12:54:14 +1100

    tests passing and renamed extension to .hpp

--- Vector.hpp
+++ Vector.hpp
@@ -6,22 +6,25 @@ attributes:
 ._size(): number of elements
 ._capacity(): number of elements holdable before array must be resized
 operator[]: indexing
-operator=: equality checking
-.front(): return v[0]
-.back(): returns v[v._size() - 1]
+operator==: equality checking
+.front(): return v[0], requires nonempty vector
+.back(): returns v[v._size() - 1] requires nonempty vector
 .push_back()
 .pop_back(): pops the last element
 .resize(): manually resizes the array
+.empty(): returns whether size of vector is 0
+.clear(): erases all elements in vector and resets capacity to MIN_CAPACITY
 */
 
 /*
 Invariants:
-- _size > 0
+- _size >= 0
 - _capacity >= _size
+- _capacity >= MIN_CAPACITY
 _size refers to number of valid elements in vector
 _capacity refers to _size of underlying array
 */
-
+#pragma once
 #include <iostream>
 #include <algorithm>
 #include <iterator>
@@ -56,7 +59,6 @@ public:
     }
     // Destructor
     ~Vector() {
-        std::cout << "Deleting vector\n" << std::endl;
         delete[] arr;
     }
     // Copy constructor
@@ -66,50 +68,36 @@ public:
             arr[i] = other.arr[i];
         }
     }
-    // Copy assignment operator, returns lvalue reference
-    // TODO: use copy-and-swap idiom
-    Vector<T>& operator=(const Vector<T>& other) {
-        if (this != &other) {
-            this->_size = other._size;
-            if (this->_capacity != other._capacity) {
-                this->_capacity = other._capacity;
-                delete[] arr;
-                this->arr = new T[_capacity];
-            }
-            // Make a deep copy of the array
-            for (int i = 0; i < _size; i++) {
-                arr[i] = other.arr[i];
-            }
-        }
-        return *this;
-    }
-    
     // Move constructor
-    Vector(Vector<T>&& other) {
-        this->_size = other._size;
-        this->_capacity = other._capacity;
-        this->arr = other.arr;
-        // Ensure other is in a valid state
+    // NOTE: I assume that Vector<T> other is move-constructed.
+    Vector(Vector<T>&& other) : _size(other._size), _capacity(other._capacity), arr(other.arr) {
+        std::cout << "Move constructor called" << std::endl;
         other._size = 0;
         other._capacity = 0;
         other.arr = nullptr;
     }
-    // Move assignment operator
-    Vector<T>& operator=(Vector<T>&& other) {
-        if (this != &other) {
-            this->_size = other._size;
-            this->_capacity = other._capacity;
-            this->arr = other.arr;
-            // Ensure other is in a valid state
-            other._size = 0;
-            other._capacity = 0;
-            other.arr = nullptr;
-        }
+    // Copy assignment operator, returns lvalue reference
+    // Copy and swap idiom: other is copied by value
+    Vector<T>& operator=(Vector<T>& other) {
+        Vector<T> temp(other);
+        swap(*this, temp);
         return *this;
+    }
+    Vector<T>& operator=(Vector<T>&& other) {
+        Vector<T> temp(std::move(other));
+        swap(*this, temp);
+        return *this;
+    }
+    friend void swap(Vector<T>& first, Vector<T>&second) {
+        // Enable ADL
+        using std::swap;
+        swap(first._size, second._size);
+        swap(first._capacity, second._capacity);
+        swap(first.arr, second.arr);
     }
 
     // Implement operator[]
-    T& operator[](int index) {
+    T& operator[](int index) const {
         if (index < 0) {
             throw std::invalid_argument("Cannot subscript into negative index");
         } else if (index >= _size) {
@@ -118,7 +106,7 @@ public:
         return arr[index];
     }
     // Implement operator<< overload as free friend function
-    friend std::ostream& operator<<(std::ostream& os, Vector& v) {
+    friend std::ostream& operator<<(std::ostream& os, Vector& v) const {
         if (v._size == 0) {
             os << "[]";
             return os;
@@ -130,7 +118,7 @@ public:
         os << v.back() << ']';
         return os;
     }
-    friend bool operator==(Vector v1, Vector v2) {
+    friend bool operator==(const Vector& v1, const Vector& v2){
         if (v1._size != v2._size) return false;
         for (int i = 0; i < v1._size; i++) {
             if (v1[i] != v2[i]) return false;
@@ -171,20 +159,29 @@ public:
         // Does not resize
         return arr[--_size];
     }
-    const T& front() {
+
+    void clear() {
+        _size = 0;
+        if (_capacity != MIN_CAPACITY) {
+            _capacity = MIN_CAPACITY;
+            delete[] arr;
+            arr = new T[MIN_CAPACITY];
+        }
+    }
+    const T& front() const {
         if (_size == 0) {
             throw std::invalid_argument("Cannot access front of empty vector");
         }
         return arr[0];
     }
-    const T& back() {
+    const T& back() const {
         if (_size == 0) {
             throw std::invalid_argument("Cannot access back of empty vector");
         }
         return arr[_size - 1];
     }
     // We don't implement iterators but return yes or no
-    bool find(const T& value) {
+    bool contains(const T& value) const {
         for (int i = 0; i < _size; i++) {
             if (arr[i] == value) {
                 return true;
@@ -192,42 +189,13 @@ public:
         }
         return false;
     }
-    bool empty() {
+    bool empty() const {
         return _size == 0;
     }
-    int size() {
+    int size() const {
         return _size;
     }
-    int capacity() {
+    int capacity() const {
         return _capacity;
     }
 };
-
-int main() {
-    // Attempt manually resizing while it's still simpler
-
-    Vector<int> v1;
-    v1.push_back(5);
-    std::cout << "First element: " << v1[0] << std::endl;
-    Vector<char> v2(3, 'a');
-    v2.push_back('b');
-    std::cout << "Vector starts and ends with" << v2.front() << v2.back() << std::endl;
-    std::cout << std::format("Popped element is {}\n", v2.pop_back());
-    std::cout << v2 << v1 << std::endl;
-    Vector<char> v3(3, 'a');
-    if (v2 == v3) {
-        std::cout <<  v2 << "==" << v3 << std::endl;
-    }
-    while (!v2.empty()) {
-        std::cout << "Popping: " << v2.pop_back() << std::endl;
-    }
-    std::cout << "Empty vector" << v2 << std::endl;
-    std::cout << std::format("v1 Size={} capacity={}\n", v1.size(), v1.capacity());
-    for (int i = 0; i < 16; i++) {
-        v1.push_back(i + 10);
-    }
-    std::cout << v1 << std::endl;
-    std::cout << std::format("v1 Size={} capacity={}\n", v1.size(), v1.capacity());
-
-
-}
