fecbf727dfa7cbc2f29a92a7bc1b5caa78c9f784
Harry Wu <u7567973@anu.edu.au>
2025-02-22 17:24:22 +0900

    added std::optional

---
+++ hashtable.cpp
@@ -0,0 +1,101 @@
+/* 
+Custom implementation of a hashtable in C++
+call it map<key, value>
+Properties:
+- needs add method: needs to overload operator[]
+- needs to value-initialize the value if operator[] looked at
+- .contains()
+- how do deal with collisions?
+-- I think we can try separate chaining
+-- alternatively, open addressing requires resizing ability
+
+How to resize a hashtable?
+allocate a space twice as large
+then, reallocate the entirety of the object pool
+- how to use the hash?
+Bonus: make it templated
+Bonus: use smart pointer
+Bonus: add locks and concurrency
+*/
+
+#include <iostream>
+#include <memory>
+#include <vector>
+#include <climits>
+#include <functional>
+constexpr int START_SIZE = 16;
+constexpr double RESIZE_THRESHOLD = 0.7;
+
+template<typename Key, typename Value>
+class Map {
+private:
+    size_t size;
+    size_t capacity;
+    std::vector<std::pair<Key, std::optional<Value>>> table;
+    // what if there is no std::has<Key>?
+    std::hash<Key> hasher;
+    size_t secondary(size_t value) {
+        // unsigned integers wrap naturally instead of throwing overflow error
+        return (value * 17 + 31) % capacity;
+    }
+    // resizing
+    void resize(int newCapacity) {
+        if (newCapacity < START_SIZE) {
+            throw std::invalid_argument("new size is too small");
+        }
+        auto new_table = std::move(table);
+        table.clear();
+        table.resize(newCapacity);
+        for (auto [k, v] : new_table) {
+            insert(k, v.value());
+        }
+    }
+public:
+    Map() : size(0), capacity(16) {}
+    void insert(const Key& k, const Value& v) {
+        size_t hash = hasher(k) % capacity;
+        while (table[hash].second) {
+            hash = secondary(hash);
+        }
+        // If I am inserting, can I use const lvalue references for k and v?
+        table[hash] = {k, v};
+        size++;
+        double load_factor = static_cast<double>(size) / capacity;
+        if (load_factor >= RESIZE_THRESHOLD) {
+            resize(2 * capacity);
+        }
+    }
+    void erase(const Key& k) {
+        size_t hash = hasher(k) % capacity;
+        while (table[hash].first != k && table[hash].second) {
+            hash = secondary(hash);
+        }
+        if (!table[hash].second.has_value()) {
+            throw std::invalid_argument("Key not found");
+        }
+        // Leaves tombstone value
+        table[hash] = {k, std::nullopt};
+        size--;
+    }
+    // TODO: how to increment size if it's an assignment?
+    Value& operator[](const Key& k) {
+        size_t hash = hasher(k) % capacity;
+        while (table[hash].second && table[hash].first != k) {
+            hash = secondary(hash);
+        }
+        if (!table[hash].second) {
+            throw std::invalid_argument("Key not found");
+        }
+        return table[hash].second.value();
+    }
+};
+
+int main() {
+    Map<std::string, int> ht;
+    ht.insert("dog", 1);
+    ht.insert("cat", 2);
+    std::cout << std::format("hashtable: dog={}\n", ht["dog"]);
+    std::cout << std::format("hashtable: cat={}\n", ht["cat"]);
+
+    return 0;
+}
